6ce66374452d391d6b761a9342a88a777407d07f
Oksana <ksushatarasenko@gmail.com>
2024-08-31 09:54:53 +0200

    lekcja 6 powtóenie

--- script.js
+++ script.js
@@ -181,3 +181,14 @@ function getDragAfterElement(containerFraz, y) {
         }
     }, { offset: Number.NEGATIVE_INFINITY }).element;
 }
+
+// Необходим скрипт, если вы хотите увеличить картинку при клике вместо наведения
+document.addEventListener('DOMContentLoaded', function () {
+    var zoomableImages = document.querySelectorAll('.zoomable');
+
+    zoomableImages.forEach(function (img) {
+        img.addEventListener('click', function () {
+            this.classList.toggle('zoomed');
+        });
+    });
+});
